80ba51a2044e52911c123ded9f6277f0ff9e0802
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-11-21 23:29:44 -0300

    react server components and client components

--- src/app/catalog/product/[...data]/page.tsx
+++ src/app/catalog/product/[...data]/page.tsx
@@ -1,18 +1,32 @@
+'use client'
+
+import { use } from 'react'
+
 interface ProductProps {
   params: Promise<{
     data: string[]
   }>
 }
 
-export default async function Product({ params }: ProductProps) {
-  const { data } = await params
+// Server Components => A gente não USA JavaScript no lado do cliente
+// Client Components => O JavaScript é enviado ao navegador (cliente)
+
+export default function Product({ params }: ProductProps) {
+  const { data } = use(params)
   const [productId, size, color] = data
+
+  console.log(params)
+  function addToCart() {
+    console.log('Adicionou ao carrinho')
+  }
 
   return (
     <div>
       <p>Product: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
